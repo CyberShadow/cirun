ab1435915f5badb69db1c258f91eefc89d33c0bd
Vladimir Panteleev <git@thecybershadow.net>
2020-04-25 11:01:09 +0000

    test/t/t-1_cli-2_integrity-2_data_job_bad: New test

---
+++ test/t/t-1_cli-2_integrity-2_job_state_bad.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+source ./lib.bash
+
+# Job data file corrupted.
+
+git init -q "$test_dir"/repo
+printf '#!/bin/sh\n\n\ntrue\n' > "$test_dir"/repo/.cirun
+chmod +x "$test_dir"/repo/.cirun
+git -C "$test_dir"/repo add -A
+git -C "$test_dir"/repo commit -qm 'Initial commit'
+commit=$(git -C "$test_dir"/repo rev-parse HEAD)
+
+"$cirun" run --job-id-file=jobid --quiet --wait repo "$test_dir"/repo "$commit"
+jobid=$(cat jobid)
+
+echo garbage > cirun-data/jobs/*/*/job.json
+
+"$cirun" status repo 2>&1 | grep -F 'Status: corrupted (Expected {, got g)'
+"$cirun" log 2>&1 | grep -F "corrupted $jobid"
