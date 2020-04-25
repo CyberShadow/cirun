c470ea400d49f89b8181e1663490f619d8e9810c
Vladimir Panteleev <git@thecybershadow.net>
2020-04-25 11:41:36 +0000

    test/t/t-1_cli-2_integrity-*: New tests

---
+++ test/t/t-1_cli-2_integrity-3_job_log_bad.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+source ./lib.bash
+
+# Job data file corrupted.
+
+git init -q "$test_dir"/repo
+printf '#!/bin/sh\n\n\ntrue\n' > "$test_dir"/repo/.cirun
+chmod +x "$test_dir"/repo/.cirun
+git -C "$test_dir"/repo add -A
+git -C "$test_dir"/repo commit -qm 'Initial commit'
+commit=$(git -C "$test_dir"/repo rev-parse HEAD)
+
+"$cirun" run --quiet --wait repo "$test_dir"/repo "$commit"
+
+echo garbage >> cirun-data/jobs/*/*/log.json
+
+diff -u <("$cirun" log repo 2>&1 | tail -n 2 | tr 0-9 '#') /dev/stdin <<'EOF'
+[##:##:## +##:##.###] Process finished with exit code #
+                      (corrupted log entry)
+EOF
---
+++ test/t/t-1_cli-2_integrity-4_global_history_bad.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+source ./lib.bash
+
+# Job data file corrupted.
+
+git init -q "$test_dir"/repo
+printf '#!/bin/sh\n\n\ntrue\n' > "$test_dir"/repo/.cirun
+chmod +x "$test_dir"/repo/.cirun
+git -C "$test_dir"/repo add -A
+git -C "$test_dir"/repo commit -qm 'Initial commit'
+commit=$(git -C "$test_dir"/repo rev-parse HEAD)
+
+"$cirun" run --job-id-file=jobid --quiet --wait repo "$test_dir"/repo "$commit"
+jobid=$(cat jobid)
+
+echo garbage >> cirun-data/history.json
+
+"$cirun" log 2>&1 | grep -F 'corrupted (corrupted global history entry)'
