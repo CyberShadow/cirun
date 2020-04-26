3d36457a85b3fedad65db9e2461c19c00396c0a1
Vladimir Panteleev <git@thecybershadow.net>
2020-04-26 15:34:39 +0000

    cirun.web.html: Initial commit

--- src/cirun/web/html.d
+++ src/cirun/web/html.d
@@ -13,5 +13,102 @@
 
 module cirun.web.html;
 
+import std.algorithm.iteration;
+import std.algorithm.sorting;
+import std.array;
+import std.conv;
+import std.range;
+
+import ae.net.http.responseex;
+import ae.sys.term;
+import ae.utils.appender;
+import ae.utils.xml.entities;
+
+import cirun.ci.job;
+import cirun.cli.term;
+import cirun.common.state;
+
 enum minimalPageTemplate = `<?content?>`;
 enum pageTemplate = import("web/page-template.html");
+
+class HTMLTerm : Term
+{
+	FastAppender!char buffer;
+
+	static HTMLTerm getInstance()
+	{
+		static HTMLTerm instance;
+		if (!instance)
+			instance = new HTMLTerm();
+		instance.buffer.clear();
+		return instance;
+	}
+
+	void putHTML(in char[] s)
+	{
+		flush();
+		buffer.put(s);
+	}
+
+	void tag(string tag, void delegate() inner)
+	{
+		flush();
+		buffer.put(`<`, tag, `>`);
+		inner();
+		flush();
+		buffer.put(`</`, tag, `>`);
+	}
+
+	void flush()
+	{
+		if (inSpan)
+		{
+			buffer.put(`</span>`);
+			inSpan = false;
+		}
+	}
+
+protected:
+	override void putText(in char[] s)
+	{
+		buffer.putEncodedEntities(s);
+	}
+
+	override void setTextColor(Color c)
+	{
+		flush();
+		if (c != Color.none)
+		{
+			buffer.put(`<span class="color-`, c.text, `">`);
+			inSpan = true;
+		}
+	}
+
+	override void setBackgroundColor(Color c) { assert(false); }
+	override void setColor(Color fg, Color bg) { assert(false); }
+
+private:
+	bool inSpan;
+}
+
+void serveIndexPage(HttpResponseEx response)
+{
+	auto t = HTMLTerm.getInstance();
+
+	auto results = getGlobalState().updateJobs();
+	t.tag(`pre`, {
+		t.put("Jobs: ");
+		foreach (g; results.map!(result => result.state.status).array.sort.group)
+			t.put(t.fg(jobStatusColor(g[0])), g[1], t.none, " ", g[0], ", ");
+		t.put(results.length, " total\n");
+
+		foreach (result; results)
+			t.printJobSummary(result);
+
+		enum numHistoryEntries = 10;
+		t.put("\nLast ", numHistoryEntries, " jobs:\n");
+		t.printGlobalHistory(getGlobalHistoryReader.reverseIter.take(numHistoryEntries));
+	});
+
+	response.writePageContents("Status", cast(string)t.buffer.get);
+}
