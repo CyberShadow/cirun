3cc9dd249e8fed31af1de93b5a96d06eb029475f
Vladimir Panteleev <git@thecybershadow.net>
2020-08-13 15:40:23 +0000

    test/t/t-5_trigger-2_event-*: New tests

---
+++ test/t/t-5_trigger-2_event-2_failed.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+source ./lib.bash
+
+# Test all event types in the job's life cycle up to "failed".
+
+git init -q "$test_dir"/repo
+printf '#!/bin/sh\n\n\nfalse\n' > "$test_dir"/repo/.cirun
+chmod +x "$test_dir"/repo/.cirun
+git -C "$test_dir"/repo add -A
+git -C "$test_dir"/repo commit -qm 'Initial commit'
+commit=$(git -C "$test_dir"/repo rev-parse HEAD)
+
+cat >> cirun.conf <<EOF
+[trigger.test]
+type = exec
+events = [queued, starting, running, succeeded, failed, errored, cancelled]
+exec.command = ["$PWD/trigger.sh"]
+EOF
+
+cat >> trigger.sh <<EOF
+#!/bin/sh
+echo "\$CIRUN_EVENT" >> "$PWD"/events.txt
+EOF
+chmod +x ./trigger.sh
+
+"$cirun" run --wait repo "$commit" --clone-url "$test_dir"/repo
+
+diff -u events.txt /dev/stdin <<EOF
+queued
+starting
+running
+failed
+EOF
